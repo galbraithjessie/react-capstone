583830e5d50d1d851796c7819e67e3b095bf9c36
Jessie Galbraith <galbraithjessie@gmail.com>
2020-11-05 13:18:13 -0700

    adjusted server.js again

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/static/'));
 app.get(/.*/, function (req, res) {
-  res.sendFile(__dirname + './dist/index.html');
+  res.sendFile(__dirname + '/static/assets/index.html');
 })
 app.listen(port);
 
